727f9f78380d29990ad6d6c98fc35eb91cf93157
Patrick Günthard <patrick.guenthard@helmedica.com>
2022-02-10 16:41:49 +0100

    Added flags to TilesPanel

--- src/java/TilesPanel.java
+++ src/java/TilesPanel.java
@@ -11,6 +11,9 @@ public class TilesPanel extends JPanel {
     private JTextField tileName;
     private List<Tile> tiles;
     private JButton addButton, removeButton;
+
+    private JCheckBox flagSlidCheckbox;
+    private JCheckBox flagWaterCheckbox;
     
     public TilesPanel(EngineDataManager dataManager) {
 	JPanel panel = new JPanel(),
@@ -34,9 +37,16 @@ public class TilesPanel extends JPanel {
 	this.tileName.setEnabled(false);
 	this.tileName.addCaretListener(e -> this.tileNameChanged());
 
+	this.flagSlidCheckbox = new JCheckBox("Solid");
+	this.flagSlidCheckbox.setEnabled(false);
+	this.flagWaterCheckbox = new JCheckBox("Water");
+	this.flagWaterCheckbox.setEnabled(false);
+	
 	panelInner.setLayout(new GridLayout(5,1));
 	panelInner.add(new JLabel("Tile Name"));
 	panelInner.add(this.tileName);
+	panelInner.add(this.flagSlidCheckbox);
+	panelInner.add(this.flagWaterCheckbox);
 	panel.add(panelInner);
 	panel.setLayout(new FlowLayout());
 	
@@ -64,13 +74,23 @@ public class TilesPanel extends JPanel {
     private void tileSelected() {
 	int index = this.tilesList.getSelectedIndex();
 	if (index > -1) {
+	    Tile t = this.tiles.get(index);
 	    this.tileName.setEnabled(true);
 	    this.removeButton.setEnabled(true);
-	    this.tileName.setText(this.tiles.get(index).getName());
+	    this.tileName.setText(t.getName());
+	    this.flagSlidCheckbox.setEnabled(true);
+	    this.flagSlidCheckbox.setSelected((t.getFlags() & 0b00000001) > 0);
+	    this.flagWaterCheckbox.setEnabled(true);
+	    this.flagWaterCheckbox.setSelected((t.getFlags() & 0b00000010) > 0);
+	    
 	} else {
 	    this.removeButton.setEnabled(false);
 	    this.tileName.setEnabled(false);
 	    this.tileName.setText("");
+	    this.flagSlidCheckbox.setEnabled(false);
+	    this.flagSlidCheckbox.setSelected(false);
+	    this.flagWaterCheckbox.setEnabled(false);
+	    this.flagWaterCheckbox.setSelected(false);
 	}
     }
 
